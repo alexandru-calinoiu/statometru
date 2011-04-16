64ecb2b33ca3471fa7d4a9bc4dc8cf34458d2f4d
calin <calinuswork@gmail.com>
2011-04-16 08:48:05 +0300

    initial commit with all the basic building blocks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statometru::Application.config.secret_token = 'd850ba8cdcf2a4c8d27b0e8fb1a4d68354bac6a301846c0fdceec01899e8e13308bed36af42e51dd13064c5bb4a0245a95cab9eaf3455247da4b6c0f54114491'
