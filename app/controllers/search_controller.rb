d9a3527111bf30749a43244a4423352e1c9175ea
Mircea Soaica <mircea@mircea-ubuntu.(none)>
2011-04-16 19:30:12 +0300

    search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,18 @@ class SearchController < ApplicationController
   end
 
   def find
-    @institutions = Institution.search("*#{params[:search_term].split.join("* *")}*", :page => params[:page], :per_page => 10)
+    @query = ""
+    if !params[:search_term].blank?
+      @search_term = params[:search_term].split
+      @query = "@name #{@search_term.join("* *")}"
+    end
+
+    if !params[:search_location].blank?
+      @search_location = params[:search_location].split
+      @query = "#{@query} @location_name #{@search_location.join("* *")}"
+    end
+
+    @institutions = Institution.search(@query, :page => params[:page], :per_page => 10, :match_mode => :extended)
     respond_to do |format|
       format.html { render :action => 'index' }
     end
