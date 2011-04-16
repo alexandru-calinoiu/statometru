4582aae4720b736478d0bd54da4909c5150f4928
Mircea Soaica <mircea@mircea-ubuntu.(none)>
2011-04-16 21:26:44 +0300

    location model update application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function(){
+    $('#location').keyup(function(){
+        $.get('/locations/autocomplete', {value: $(this).val()}, function(data){
+
+        });
+    });
+
+    $('#institution').keyup(function(){
+        $.get('/institutions/find', {search_term: $(this).val(), search_location: $('#location').val()}, function(data){
+            $('#content').html(data);
+        })
+    });
+});
