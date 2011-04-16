50d7d4b4364c38aee91fe22a04686684b6efac59
Mircea Soaica <mircea@mircea-ubuntu.(none)>
2011-04-16 15:17:00 +0300

    search stuff

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def index
+    @institutions = Institution.paginate(:page => params[:page], :per_page => 16)
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def search
+  end
+
+end
