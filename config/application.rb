6d20d1c407d88cf4dfa59961de6cc3e22bc83089
calin <calinuswork@gmail.com>
2011-04-17 00:03:31 +0300

    merged with directory changes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Statometru
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directories are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
